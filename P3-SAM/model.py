addab37f2062f8efd7bdd5fabedd007279a7acae
CrackerHax <CrackerHax@users.noreply.github.com>
2025-09-28 05:40:11 -0700

    Update model.py

--- P3-SAM/model.py
+++ P3-SAM/model.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'XPart/partgen'))
 from models import sonata
 from utils.misc import smart_load_model
+from pathlib import Path
 
 '''
 This is the P3-SAM model.
@@ -16,6 +17,7 @@ The model is composed of three parts:
 def build_P3SAM(self): #build p3sam
     ######################## Sonata ########################
     #self.sonata = sonata.load("sonata", repo_id="facebook/sonata", download_root='/root/sonata')
+    
     self.sonata = sonata.load("sonata", repo_id="facebook/sonata", download_root=str(Path.home() / '.cache' / 'sonata'))
     self.mlp = nn.Sequential(
             nn.Linear(1232, 512),
